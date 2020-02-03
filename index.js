edc31fc7d9af8c9d54108f6105bb6a71d9551d71
Archan Patkar <pat@archanpatkar.com>
2020-02-03 12:27:11 +0530

    Update

--- index.js
+++ index.js
@@ -1,183 +1,108 @@
+const mure = {}
+const M = 'M'
+const U = 'U'
+const I = 'I'
+const EMPTY = ''
+const SPACE = ' '
+const symbols = [M, I, U, EMPTY, SPACE]
+const rule3patt = /III/gi
+const rule4patt = /UU/gi
 
-let M = "M";
-let U = "U";
-let I = "I";
-let EMPTY = "";
+mure.axiom = 'MI'
 
-var rule3patt = /III/gi;
-var rule4patt = /UU/gi;
-
-let rules =  {
-    1: str => {
-        if(str.endsWith(I)) return true
-    },
-    2: str => {
-        if(str[0] == M) return true;
-    },
-    3: str => {
-        if(str.match(rule3patt) != undefined) return true;
-    },
-    4: str => {
-        if(str.match(rule4patt) != undefined) return true;
+mure.isValid = (str) => {
+  for (const ch in str) {
+    if (!(ch in symbols)) {
+      throw new SyntaxError('Invalid MIU String')
     }
+  }
+  return true
 }
 
-function canApply(str,rule) {
-    if(rule in rules) return rules[rule](str)?true:false;
+mure.rules = {
+  1: str => {
+    if (str.endsWith(I)) return true
+  },
+  2: str => {
+    if (str[0] === M) return true
+  },
+  3: str => {
+    if (str.match(rule3patt) !== undefined) return true
+  },
+  4: str => {
+    if (str.match(rule4patt) !== undefined) return true
+  }
 }
 
-function canAppyHowMany(str)
-{
-    let out = [];
-    for(let rule in rules){
-        if(canApply(str,rule))
-        {
-            out.push(rule);
-        }
-    } 
-    return out;
+mure.apply = {
+  1: str => {
+    return (str += U)
+  },
+  2: str => {
+    const x = str.slice(1)
+    str += x
+    return str
+  },
+  3: str => {
+    return str.replace(rule3patt, U)
+  },
+  4: str => {
+    return str.replace(rule4patt, EMPTY)
+  }
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+Object.freeze(mure.rules)
+Object.freeze(mure.apply)
+
+mure.canApply = (str, rule) => {
+  if (rule in mure.rules) return !!mure.rules[rule](str)
 }
 
-function selectRandomRule(str)
-{
-    let rules = canAppyHowMany(str);
-    return rules[getRandomInt(rules.length)];
-}
-
-let apply =  {
-    1: str => {
-        return str+=U;
-    },
-    2: str => {
-        let x = str.slice(1);
-        str += x;
-        return str;
-    },
-    3: str => {
-        return str.replace(rule3patt,U);
-    },
-    4: str => {
-        return str.replace(rule4patt,EMPTY);
+mure.canApplyWhich = (str) => {
+  const out = []
+  for (const rule in mure.rules) {
+    if (mure.canApply(str, rule)) {
+      out.push(rule)
     }
+  }
+  return out
 }
 
-function applyRule(str,rule)
-{
-    if(rule in apply) return apply[rule](str);
+mure.applyRule = (str = mure.axiom, rule) => {
+  if (rule in mure.apply) return mure.apply[rule](str)
 }
 
-function findold(start,end,iterations)
-{
-    console.log("Randomized Bruteforce Solution for MU Puzzle");
-    console.log("Starting String: " + start);
-    console.log("To Find: " + end);
-    let main = start;
-    let i = 0;
-    while(i < iterations)
-    {
-        console.log("Iteration: " + (i+1));
-        let rule = getRandomInt(4)+1;   
-        if(rule != undefined)
-        {
-            console.log("Before Rule Application: " + main);
-            console.log("Applying Rule: " + rule);
-            main = applyRule(main,rule);
-            console.log("After Rule Application: " + main);
+mure.possibilityTree = (iterations, start = mure.axiom) => {
+  console.log('Starting String: ' + start)
+  let temp = []
+  let main
+  let total
+  if (Array.isArray(start)) {
+    main = [...start]
+    total = [...start]
+  } else {
+    main = [start]
+    total = [start]
+  }
+  let i = 0
+  while (i < iterations) {
+    for (const m of main) {
+      const rules = mure.canApplyWhich(m)
+      if (rules !== undefined) {
+        for (const rule of rules) {
+          const val = mure.applyRule(m, rule)
+          if (!(val in temp)) temp.push(val)
         }
-        i++;
+      }
     }
-}
-
-function possibliltyTree(start,iterations)
-{
-    console.log("Starting String: " + start);
-    let main = [start];
-    let temp = [];
-    let i = 0;
-    while(i < iterations) {
-        console.log("Iteration: " + (i+1));
-        console.log(main);
-        for(let m of main) {
-            let rules = canAppyHowMany(m);   
-            if(rules != undefined) {
-                for(let rule of rules) {
-                    const val = applyRule(m,rule);
-                    if(!(val in temp)) temp.push(val);
-                }
-            }
-        }
-        if(temp.length > 0) {
-            main = temp;
-            temp = [];
-        }
-        i++;
+    if (temp.length > 0) {
+      main = temp
+      total.push(Array(...main))
+      temp = []
     }
-    return main;
+    i++
+  }
+  return total
 }
 
-function find(start,end,iterations)
-{
-    console.log("Randomized Bruteforce Solution for MU Puzzle");
-    console.log("Starting String: " + start);
-    console.log("To Find: " + end);
-    let order = [];
-    let output = [start];
-    let main = start;
-    let i = 0;
-    while(i < iterations && main != end)
-    {
-        console.log("Iteration: " + (i+1));
-        let rule = selectRandomRule(main);   
-        if(rule != undefined)
-        {
-            console.log("Before Rule Application: " + main);
-            console.log("Applying Rule: " + rule);
-            order.push(rule);
-            main = applyRule(main,rule);
-            console.log("After Rule Application: " + main);
-        }
-        output.push(main);
-        i++;
-    }
-    return [order,output];
-}
-
-
-function find2(start,end,iterations)
-{
-    console.log("Randomized Bruteforce Solution for MU Puzzle");
-    console.log("Starting String: " + start);
-    console.log("To Find: " + end);
-    let order = [];
-    let output = [start];
-    let main = start;
-    let i = 0;
-    while(i < iterations && main != end)
-    {
-        console.log("Iteration: " + (i+1));
-        let rules = canAppyHowMany(main);   
-        if(rules != undefined)
-        {
-            for(let rule of rules) {
-                console.log("Before Rule Application: " + main);
-                console.log("Applying Rule: " + rule);
-                order.push(rule);
-                main = applyRule(main,rule);
-                console.log("After Rule Application: " + main);
-            }
-        }
-        output.push(main);
-        i++;
-    }
-    return [order,output];
-}
-
-let problem = "MI";
-let tofind = "MII";
-
-console.log(possibliltyTree(problem,12));
-// console.log(find2(problem,tofind,6));
+module.exports = Object.freeze(mure)
