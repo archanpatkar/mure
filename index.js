4fa2ccbbb4cb65d093e3e41659da483852f75d53
Archan Patkar <pat@archanpatkar.com>
2020-02-22 23:18:40 +0530

    More Compacting in progress

--- index.js
+++ index.js
@@ -15,49 +15,46 @@ mure.isValid = str => [...str].reduce((o,ch) => symbols.includes(ch)&&o,true);
 mure.rules = [
   str => str.endsWith(I),
   str => str[0] === M,
-  str => str.match(rule3patt) !== null, // Replace with matchall
-  str => str.match(rule4patt) !== null // Replace with matchall
+  str => str.match(rule3patt) !== null,
+  str => str.match(rule4patt) !== null
 ]
 
 mure.apply = [
   str => (str += U),
   str => (str += str.slice(1)),
-  str => str.replace(rule3patt, U), // Replace with matchall
-  str => str.replace(rule4patt, EMPTY) // Replace with matchall
-];
-
-Object.freeze(mure.rules)
-Object.freeze(mure.apply)
+  str => {
+    // Array.from(str.matchAll(rule4patt)).map(match =>
+    //   Array.from(match.input)
+    //   .map((ch,i) => i >= match.index && i <= match.index+2?U:ch)
+    //   .join("")
+    // )
+    // str.replace(rule3patt, U)
+  }, 
+  str => Array.from(str.matchAll(rule4patt)).map(match =>
+            Array.from(match.input)
+            .filter((ch,i) => i < match.index || i > match.index+1)
+            .join("")
+        )
+]
 
 mure.canApply = (str, rule) => {
   if(!mure.isValid(str)) throw new SyntaxError('Invalid MIU String')
-  else if (rule-1 in mure.rules) return mure.rules[rule-1](str)
+  else if (rule-1 in mure.rules) return mure.rules[rule-1](str.toUpperCase())
 }
 
 mure.canApplyWhich = str => mure.rules.flatMap((v,i) => mure.canApply(str, i+1)?[i+1]:[]);
 
 mure.applyRule = (str = mure.axiom, rule) => {
   if(!mure.isValid(str)) throw new SyntaxError('Invalid MIU String')
-  if(rule-1 in mure.apply) return mure.apply[rule-1](str)
+  if(rule-1 in mure.apply) return mure.apply[rule-1](str.toUpperCase())
 }
 
-mure.possibility = (iterations, start = mure.axiom) => {
-  let temp = []
-  let main
-  let total
-  if (Array.isArray(start)) {
-    main = [...start]
-    total = [start]
-  } else {
-    main = [start]
-    total = [start]
-  }
-  for(let i = 0;i < iterations;i++) {
-    total.push(Array.from(main = main.map(
-      m => mure.canApplyWhich(m).map((rule) => mure.applyRule(m, rule))
-    ).flat()));
-  }
-  return total
-}
+mure.possibility = (iterations, start = mure.axiom) => Array(iterations).fill(0).reduce((p,c) =>
+      !p[1].push(Array.from(p[0] = p[0].map(
+        m => mure.canApplyWhich(m).map(rule => mure.applyRule(m, rule))
+      ).flat(2))) && p,
+      [Array.isArray(start)?[...start]:[start],[start]])[1];
 
+Object.freeze(mure.apply)
+Object.freeze(mure.rules)
 module.exports = Object.freeze(mure)
--- test.js
+++ test.js
@@ -2,14 +2,19 @@ const mure = require('./index')
 
 const str = 'MII'
 
+// console.log("MIIIIII");
+// console.log([..."MIIIIII".matchAll(rule3patt)]);
 
-console.log(mure.isValid(str));
-console.log(mure.isValid("archan"));
-console.log(mure.isValid("MI"));
+console.log(mure.applyRule("MIIIUUIUuuIII",3));
+
+
+// console.log(mure.isValid(str));
+// console.log(mure.isValid("archan"));
+// console.log(mure.isValid("MI"));
 // let t = mure.canApplyWhich(str);
 // console.log(t);
 // t.forEach(f => console.log(f.toString()));
 
 // console.log(mure.applyRule(mure.applyRule(str, 2), 3))
 
-console.log(mure.possibility(3,str))
+// console.log(mure.possibility(3,str))
