0c6341c1275f98360e7f7da9c55c1a126a5eb8d8
Archan Patkar <pat@archanpatkar.com>
2020-02-26 12:49:31 +0530

    Formatting

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const mure = {}
 const symbols = ['M', 'I', 'U', '', ' ']
-const patt = [/III/gi,/UU/gi]
-const [M,I,U,EMPTY,SPACE] = symbols;
-const error_gen = ()=>{throw new SyntaxError('Invalid MIU String')};
-const app = obj => (str=mure.axiom,rule) => mure.isValid(str) && (rule-1 in obj)?obj[rule-1](str.toUpperCase()):error_gen();
+const patt = [/III/gi, /UU/gi]
+const [M, I, U, ...extra] = symbols
+const errorGen = () => { throw new SyntaxError('Invalid MIU String') }
+const app = obj => (str = mure.axiom, rule) => mure.isValid(str) && (rule - 1 in obj) ? obj[rule - 1](str.toUpperCase()) : errorGen()
 
 mure.axiom = 'MI'
 
-mure.isValid = str => [...str].reduce((o,ch) => symbols.includes(ch.toUpperCase())&&o,true);
+mure.isValid = str => [...str].reduce((o, ch) => symbols.includes(ch.toUpperCase()) && o, true)
 
 mure.rules = [
   str => str.endsWith(I),
@@ -19,27 +19,26 @@ mure.rules = [
 mure.apply = [
   str => (str += U),
   str => (str += str.slice(1)),
-  str => Array.from(str.matchAll(patt[0])).map((match,a,b,c,o = false) => 
+  str => Array.from(str.matchAll(patt[0])).map((match, a, b, c, o = false) =>
     Array.from(match.input)
-    .map((ch,i) => i >= match.index && i <= match.index+2?!o?((o=true)&&U):'':ch)
-    .join("")
-  ), 
+      .map((ch, i) => i >= match.index && i <= match.index + 2 ? !o ? ((o = true) && U) : '' : ch)
+      .join('')
+  ),
   str => Array.from(str.matchAll(patt[1])).map(match =>
     Array.from(match.input)
-    .filter((ch,i) => i < match.index || i > match.index+1)
-    .join("")
+      .filter((ch, i) => i < match.index || i > match.index + 1)
+      .join('')
   )
 ]
 
-mure.canApply = app(mure.rules);
-mure.applyRule = app(mure.apply);
-mure.canApplyWhich = str => mure.rules.flatMap((v,i) => mure.canApply(str, i+1)?[i+1]:[]);
-
-mure.possibility = (iterations, start=mure.axiom) => Array(iterations).fill(0).reduce(p => 
+mure.canApply = app(mure.rules)
+mure.applyRule = app(mure.apply)
+mure.canApplyWhich = str => mure.rules.flatMap((v, i) => mure.canApply(str, i + 1) ? [i + 1] : [])
+mure.possibility = (iterations, start = mure.axiom) => Array(iterations).fill(0).reduce(p =>
   !(p[1].push(Array.from(p[0] = p[0].map(
     m => mure.canApplyWhich(m).map(rule => mure.applyRule(m, rule))
-  ).flat(2))))?p:p,
-  [Array.isArray(start)?[...start]:[start],[start]])[1];
+  ).flat(2)))) ? p : p,
+[Array.isArray(start) ? [...start] : [start], [start]])[1]
 
 Object.freeze(mure.apply)
 Object.freeze(mure.rules)
--- test.js
+++ test.js
@@ -1,20 +1,2 @@
 const mure = require('./index')
-
-const str = 'MII'
-
-// console.log("MIIIIII");
-// console.log([..."MIIIIII".matchAll(rule3patt)]);
-
-// console.log(mure.applyRule("MIIIUUIUuuUUIII",4));
 console.log(mure.possibility(4))
-// console.log(mure.applyRule("MIIIIIIUIIUIII",3));
-// console.log(mure.isValid(str));
-// console.log(mure.isValid("archan"));
-// console.log(mure.isValid("MI"));
-// let t = mure.canApplyWhich(str);
-// console.log(t);
-// t.forEach(f => console.log(f.toString()));
-
-// console.log(mure.applyRule(mure.applyRule(str, 2), 3))
-
-// console.log(mure.possibility(3,str))
