37b3f9ae2d440827afc33540384798e5541baca4
Archan Patkar <pat@archanpatkar.com>
2020-03-05 17:54:34 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const patt = [/III/gi, /UU/gi]
 const [M, I, U, ...extra] = symbols
 const errorGen = () => { throw new SyntaxError('Invalid MIU String') }
 const app = obj => (str = mure.axiom, rule) => mure.isValid(str) && (rule - 1 in obj) ? obj[rule - 1](str.toUpperCase()) : errorGen()
-mure.isValid = str => [...str].reduce((o, ch) => symbols.includes(ch.toUpperCase()) && o, true)
-mure.matchAll = (str, ch, times) => [...str].reduce((acc, v, i) => v === ch
+const matchAll = (str, ch, times) => [...str].reduce((acc, v, i) => v === ch
   ? Array(times).fill(0).reduce((acc, _, k) => str[i + k] === ch, true)
     ? !acc.push({ index: i, input: str }) ? acc : acc : acc : acc, [])
+mure.isValid = str => [...str].reduce((o, ch) => symbols.includes(ch.toUpperCase()) && o, true)
 mure.rules = [
   str => str.endsWith(I),
   str => str[0] === M,
@@ -17,11 +17,11 @@ mure.rules = [
 mure.apply = [
   str => (str += U),
   str => (str += str.slice(1)),
-  str => mure.matchAll(str, I, 3).map((match, a, b, c, o = false) => Array.from(match.input)
+  str => matchAll(str, I, 3).map((match, a, b, c, o = false) => Array.from(match.input)
     .map((ch, i) => (i >= match.index && i <= match.index + 2) ? (!o ? ((o = true) && U) : '') : ch)
     .join('')
   ),
-  str => mure.matchAll(str, U, 2).map(match =>
+  str => matchAll(str, U, 2).map(match =>
     Array.from(match.input)
       .filter((ch, i) => i < match.index || i > match.index + 1)
       .join('')
