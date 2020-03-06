f3868c5997506b565e2cd15698e17079e1fd66d7
Archan Patkar <pat@archanpatkar.com>
2020-03-06 23:57:39 +0530

    Update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,3 +11,21 @@ test('mure.isValid', () => {
     expect(mure.isValid("zzzZzZ")).toBe(false);
 });
 
+test('mure.canApply', () => {
+    expect(mure.canApply("MI",1)).toBe(true);
+    expect(mure.canApply("MI",2)).toBe(true);
+    expect(mure.canApply("MI",3)).toBe(false);
+    expect(mure.canApply("MI",4)).toBe(false);
+    expect(mure.canApply("MUU",1)).toBe(false);
+    expect(mure.canApply("MUU",2)).toBe(true);
+    expect(mure.canApply("MUU",3)).toBe(false);
+    expect(mure.canApply("MUU",4)).toBe(true);
+    expect(mure.canApply("MIII",1)).toBe(true);
+    expect(mure.canApply("MIII",2)).toBe(true);
+    expect(mure.canApply("MIII",3)).toBe(true);
+    expect(mure.canApply("MIII",4)).toBe(false);
+    expect(mure.canApply("MUI",1)).toBe(true);
+    expect(mure.canApply("MUI",2)).toBe(true);
+    expect(mure.canApply("MUI",3)).toBe(false);
+    expect(mure.canApply("MUI",4)).toBe(false);
+});
--- demo.js
+++ demo.js
@@ -1,10 +1,8 @@
 const mure = require('./src/index')
 
 console.log("MIIIIIII");
-console.log(mure.matchAll("MIIIIIII","I",3));
 console.log(mure.applyRule("MIIIIIII",3));
 console.log("MIIUUIUUIUUUUUUI");
-console.log(mure.matchAll("MIIUUIUUIUUUUUUI","U",2));
 console.log(mure.applyRule("MIIUUIUUIUUUUUUI",4));
 console.log("Possibility: MI");
 console.log(mure.possibility(5));
