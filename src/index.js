d88d851e88fc7b578a174ce44b4980a94cccb814
Archan Patkar <pat@archanpatkar.com>
2020-03-12 21:32:34 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -21,14 +21,13 @@ const apply = [
       .join('')
   )
 ]
-const synerr = () => { throw new Error('Invalid MIU String') }
-const rulerr = () => { throw new Error('Invalid Rule') }
+const err = (i) => { if(i) throw new Error('Invalid MIU String'); else throw new Error('Invalid Rule') }
 const matchAll = (str, ch, times) => [...str].reduce((acc, v, i) => v === ch
   ? Array(times).fill(0).reduce((acc, _, k) => str[i + k] === ch, true)
     ? !acc.push({ index: i, input: str }) ? acc : acc : acc : acc, [])
 mure.isValid = str => [...str].reduce((o, ch) => symbols.includes(ch.toUpperCase()) && o, true)
-mure.canApply = (str, rule) => (rule - 1 in rules) ? rules[rule - 1](str.toUpperCase()) : rulerr()
-mure.applyRule = (str, rule) => mure.isValid(str) ? mure.canApply(str, rule) ? apply[rule - 1](str.toUpperCase()) : rulerr() : synerr()
+mure.canApply = (str, rule) => (rule - 1 in rules) ? rules[rule - 1](str.toUpperCase()) : err(0)
+mure.applyRule = (str, rule) => mure.isValid(str) ? mure.canApply(str, rule) ? apply[rule - 1](str.toUpperCase()) : err(0) : err(1)
 mure.canApplyWhich = str => rules.flatMap((v, i) => mure.canApply(str, i + 1) ? [i + 1] : [])
 mure.possibility = (iterations, start = mure.axiom) => Array(iterations).fill(0).reduce(p =>
   p[1].add(new Set(p[0] = p[0].map(
