e8257cc7e016d2418c7e367e843bbc3b18ccda38
Archan Patkar <pat@archanpatkar.com>
2020-03-13 09:55:52 +0530

    More testing and formatting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const mure = { axiom: 'MI' }
 const symbols = ['M', 'I', 'U', '', ' ']
 const patt = [/III/gi, /UU/gi]
 const [M, I, U, ...extra] = symbols
+const err = (i) => { if (i) throw new Error('Invalid MIU String'); else throw new Error('Invalid Rule') }
+const matchAll = (str, ch, times) => [...str].reduce((acc, v, i) => v === ch
+  ? Array(times).fill(0).reduce((acc, _, k) => str[i + k] === ch, true)
+    ? !acc.push({ index: i, input: str }) ? acc : acc : acc : acc, [])
 const rules = [
   str => str.endsWith(I),
   str => str[0] === M,
@@ -12,7 +16,7 @@ const apply = [
   str => (str += U),
   str => (str += str.slice(1)),
   str => matchAll(str, I, 3).map((match, a, b, c, o = false) => Array.from(match.input)
-    .map((ch, i) => i >= match.index && i <= match.index + 2 ? !o ? o = true && U : '' : ch)
+    .map((ch, i) => i >= match.index && i <= match.index + 2 ? !o ? (o = true) && U : '' : ch)
     .join('')
   ),
   str => matchAll(str, U, 2).map(match =>
@@ -21,10 +25,6 @@ const apply = [
       .join('')
   )
 ]
-const err = (i) => { if(i) throw new Error('Invalid MIU String'); else throw new Error('Invalid Rule') }
-const matchAll = (str, ch, times) => [...str].reduce((acc, v, i) => v === ch
-  ? Array(times).fill(0).reduce((acc, _, k) => str[i + k] === ch, true)
-    ? !acc.push({ index: i, input: str }) ? acc : acc : acc : acc, [])
 mure.isValid = str => [...str].reduce((o, ch) => symbols.includes(ch.toUpperCase()) && o, true)
 mure.canApply = (str, rule) => (rule - 1 in rules) ? rules[rule - 1](str.toUpperCase()) : err(0)
 mure.applyRule = (str, rule) => mure.isValid(str) ? mure.canApply(str, rule) ? apply[rule - 1](str.toUpperCase()) : err(0) : err(1)
