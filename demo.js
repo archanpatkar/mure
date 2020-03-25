9eebdebb7a23894803b892fbf228866045e5ab0c
Archan Patkar <archanpatkar@gmail.com>
2020-03-25 23:39:03 +0530

    Cleanup

--- demo.js
+++
@@ -1,8 +0,0 @@
-const mure = require('./src/index')
-
-console.log("MIIIIIII");
-console.log(mure.applyRule("MIIIIIII",3));
-console.log("MIIUUIUUIUUUUUUI");
-console.log(mure.applyRule("MIIUUIUUIUUUUUUI",4));
-console.log("Possibility: MI");
-console.log(mure.possibility(5));
